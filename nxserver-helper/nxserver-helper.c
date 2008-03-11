ed6d0c3eb4b7c1fbad7732ec330442d593b9f782
fabianx <>
2008-03-11 05:55:27 +0000

    * Made the slave mode finally functional. With that slave mode it is  possible to do a single sign on instead of the multiple logins used  before. It is also possible to use a suid wrapper to login as user.  With single sign on session startup is a lot faster. This is true  especially if there are many printers and files to be shared. (fabianx@bat.berlios.de)
    This is also the basis for importing nxserver_suid from redesign.

--- nxserver-helper/nxserver-helper.c
+++ nxserver-helper/nxserver-helper.c
@@ -3,19 +3,34 @@
  *
  * License: GPL, v2
  *
- * SVN: $Id: nxserver-helper.c 370 2007-08-12 05:45:19Z fabianx $
+ * SVN: $Id: nxserver-helper.c 517 2008-03-11 05:55:27Z fabianx $
  *
  */
 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[])
 {
 	int fds[2];
+	char buf[1024];
 
 	socketpair(AF_UNIX, SOCK_STREAM, 0, fds);
+	
+	// This fd can be used by the server for spawning the node
+	snprintf(buf, 1022, "%d", fds[1]);
+	setenv("NX_SERVER_FD", buf, 1);
+	
+	// This fd can be used for communication with the node
+	snprintf(buf, 1022, "%d", fds[0]);
+	setenv("NX_COMM_FD", buf, 1);
+
+	// We do not trust this user, he still has to login
+	unsetenv("NX_TRUSTED_USER");
+
 	argv++;
 	execv(argv[0], argv);
 }
